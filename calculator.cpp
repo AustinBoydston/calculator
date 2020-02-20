ed15e5f1afd50d9522bd524a3de5b3334a809acc
AustinBoydston <austin.t.boydston-1@ou.edu>
2020-02-19 20:34:59 -0600

    succesful build

---
+++ calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main (int size, char *array[])
+{
+
+
+
+
+int sum = 0; 
+
+
+int temp = 0;
+  for(int i = 0; i < size; i++)
+     {
+
+  if(array[i] == " ")
+	{
+	i--;
+	}
+	else if(array[i] == "-")
+	{
+	sum -= temp;
+	i++;
+	i++;		
+	}
+  else 
+	{ 
+	temp = atoi(array[i]);
+	sum += temp;
+	}
+
+
+    }
+
+cout << sum << endl;
+
+return sum;
+}
+
+
+
