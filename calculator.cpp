c03258e78155792f78cf8ff9c7144f393e78c916
AustinBoydston <austin.t.boydston-1@ou.edu>
2020-03-24 14:37:31 -0500

    Completed

--- calculator.cpp
+++ calculator.cpp
@@ -1,161 +1,136 @@
 #include <iostream>
 using namespace std;
  
-int add(float n1, float n2);
-int sub(float n1, float n2);
-int mult(float n1, float n2);
-int div(float n1, float n2);
-int main (int size, char *array[])
+
+
+
+double firstOp;
+double secondOp;
+bool oddCase;
+////////////////////////////////////////////////////////Method implementaiton//////////////////////////////////////////////////
+
+double addition(double firstOperand, double secondOperand)
+{
+double sum = firstOperand + secondOperand;
+return sum;
+}
+
+double subtract (double firstOperand, double secondOperand)
+{
+double diff = 0.0;
+ diff = firstOperand - secondOperand;
+ 
+ return diff;
+}
+
+double multiply (double firstOperand, double secondOperand)
+{
+double mult = firstOperand * secondOperand;
+return mult; 
+}
+
+double divide (double firstOperand, double secondOperand)
+{
+double div = firstOperand / secondOperand;
+return div;
+}
+
+int selectOperation(char candidateOperator)
 {
 
-bool oddCase = false;
+
+switch(candidateOperator)
+{
+case '+' : return 1;       break;
+case '-': return 2;      break;
+case '*': return 3;      break;
+case '/': return 4;      break;
+default :  cout << "Please select +, -, *, or / for the second argument." << endl; return -2; break;
+
+}
+}
+////////////////////////////////////////////////////Main Method///////////////////////////////////////////
+int main (int argc, char ** argv)
+{
+double firstOperand;
+double secondOperand;
+double result;
+
+ oddCase = false;
 
 
-//cout << size << endl;
-if(size != 4 && (size != 5 && array[2] != "*"))
+
+if(argc != 4 && (argc != 5 && argv[2] != "*"))
 {
 	cout << "Error: Invalid Input: Please input exactly three parameters" << endl;
 	return -3;
 }
-//else if(size != 5 && array[2] != "*")
 
-//cout << array[4] << endl;
 
-//cout << array[3] << endl;
-
-//cout << array[2] << endl;
-
-//cout << array[1] << endl;
-
-//if(atof(array[1]))
-if(size != 5)
+if(argc != 5)
 {
-if(atof(array[1]) == 0 || atof(array[3]) == 0)
+if(atof(argv[1]) == 0 || atof(argv[3]) == 0)
 {
 	cout << "Zero is not allowed for either operand!" << endl;
 	return -1;
 }
 }
-else if(size == 5)
+else if(argc == 5)
 {
 	oddCase = true;
- if(atof(array[4]) == 0)
+ if(atof(argv[4]) == 0)
 {
 cout << "Zero is not allowed for either operand!" << endl;
 	return -1;
 
 }
 }
-float n1 = atof(array[1]);
-float n3 = atof(array[3]);
+ firstOperand = (double) atof(argv[1]);
+ secondOperand = (double) atof(argv[3]);
+ char holdThis = *argv[2];
 if(oddCase)
 {
-	n3 = atof(array[4]);
-}
-
-/*
-switch(array[2])
-{
-case "+": return add (n1, n3); break;
-case "-": return sub (n1, n3); break;
-case "*": return mult(n1, n3); break;
-case "/": return div (n1, n3); break;
-default: cout << "Please select +, -, *, or / for the second argument." << endl;
-return -2;
-break;
-}*/
-char holdThis = *array[2];
-
-if(holdThis == '+')
-{
-return add(n1, n3);
-}
-else if(holdThis == '-')
-{
-return sub(n1, n3);
-}
-else if(holdThis == '*' || oddCase)
-{
-	if(oddCase)
+	cout << argv[3] << endl;;
+	secondOperand = (double) atof(argv[4]);
+	holdThis = '*';//*argv[2];
+	
+	
+	if(argv[3] != "calculator.cpp")
 	{
-		return mult(n1, n3);
-	}
-
-return mult(n1, n3);
-}
-else if(holdThis == '/')
-{
-return div(n1, n3);
-}
-else
-{
-	{
-cout << "Please select +, -, *, or / for the second argument." << endl;
-return -2;
+		cout << "Error: Invalid Input: Please input exactly three parameters" << endl;
+	return -3;
 	}
 }
 
 
-}
-////////////////////////////////////////////////////////Method implementaiton//////////////////////////////////////////////////
-int add(float n1, float n2)
+
+
+int temp = selectOperation(holdThis);
+
+if(temp == 1)
 {
-float sum = 0; 
-
-/*
-int temp = 0;
-  for(int i = 0; i < size; i++)
-     {
-
-  if(array[i] == " ")
-	{
-	i--;
-	}
-	else if(array[i] == "-")
-	{
-	sum -= temp;
-	i++;
-	i++;		
-	}
-  else 
-	{ 
-	temp = atoi(array[i]);
-	sum += temp;
-	}
-
-
-    }
-*/
-
-sum = n1 + n2;
-cout << sum << endl;
-
-return  0;
+cout << addition(firstOperand, secondOperand)  << endl;  
+ return 0;   
 }
-
-int sub(float n1, float n2)
+else if(temp == 2)
 {
-float diff = 0.0;
- diff = n1 - n2;
- cout << diff << endl;
- return 0;
+cout << subtract(firstOperand, secondOperand)  << endl;   
+return 0;   
 }
-
-int mult(float n1, float n2)
+else if(temp == 3)
 {
-	float mult = 0.0;
-	mult = n1 * n2;
-	cout << mult << endl;
-	return 0;
+cout << multiply(firstOperand, secondOperand)  << endl;   
+return 0;   
 }
-
-int div(float n1, float n2)
+else if(temp == 4)
 {
-	float div = 0.0;
-	div = n1 / n2;
-	cout << div << endl;
-	return 0;
+cout << divide(firstOperand, secondOperand)    << endl;   
+return 0;   
 }
 
 
+ cout << "Please select +, -, *, or / for the second argument." << endl; 
+ return -2;
+	
+}
 
